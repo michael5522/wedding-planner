0919fae663697d82b6d9d9cbab264a8119f51b31
Michael Chang <michaelchang.kuo@gmail.com>
2022-10-19 02:20:50 +0000

    finished feature1 can view home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,12 +5,11 @@ export default function Home(props) {
     <div className="container">
       <div className="row mb-2">
         <div className="col">
-          <h2>Online</h2>
+          <h2 className="mt-1">Online</h2>
           <h2>Wedding Planning</h2>
           <h2>Assistant</h2>
-          <h5>Get started with planning</h5>
-          <h2><br /> </h2>
-          <a href="#login" className="btn btn-outline-secondary" role="button">Sign up for free!</a>
+          <h5 className="mb-3">Get started with planning</h5>
+          <a href="#login" className="btn btn-outline-secondary mt-2" role="button">Sign up for free!</a>
 
         </div>
         <div className="col d-flex align-items-center">
@@ -18,18 +17,18 @@ export default function Home(props) {
         </div>
       </div>
       <div className="row">
-        <h3>Example Benefits of Platform:</h3>
+        <h3>Example Benefits:</h3>
       </div>
-      <div className="row">
+      <div className="row bg-light">
         <div className="col-md-4 border rounded">
           <h4>Guest List Manager</h4>
-          <h6>Simplify address collection</h6>
+          <h6 className="italic">Simplify address collection</h6>
           <p className="text-justify indent"><br />Whether you’re having a wedding shower, bach party, engagement party or rehearsal dinner, our guest list manager was thoughtfully designed to help you gather guest info and keep it organized for any and all of your events.</p>
         </div>
 
         <div className="col-md-4 border rounded">
           <h4>Wedding Checklist</h4>
-          <h6>A Wedding To-Do List</h6>
+          <h6 className="italic">A Wedding To-Do List</h6>
           <p className="text-justify"><br />Add. Remove. Update. <br />
             Your wedding to-do list is what you make it. <br />
             Easily update or change to fit your needs.</p>
@@ -37,7 +36,7 @@ export default function Home(props) {
 
         <div className="col-md-4 border rounded">
           <h4>Budgeter</h4>
-          <h6>Set up your Budget!</h6>
+          <h6 className="italic">Set up your Budget!</h6>
           <p className="text-justify"><br />Stay on top of yours spending - keep track of your total budget, prices, deposits, and any outstanding fees</p>
         </div>
       </div>
