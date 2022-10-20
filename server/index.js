600cdfa8f949f7a9458cd51791f7eb9241fd4b65
Michael Chang <michaelchang.kuo@gmail.com>
2022-10-20 00:42:15 +0000

    added sign in server side

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv/config');
 const pg = require('pg');
 const argon2 = require('argon2'); // eslint-disable-line
 const express = require('express');
+const jwt = require('jsonwebtoken'); // eslint-disable-line
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
@@ -25,8 +26,8 @@ app.get('/api/hello', (req, res) => {
 });
 
 // registration
-app.post('/api/register', (req, res, next) => {
-  console.log('post register');
+app.post('/api/auth/sign-up', (req, res, next) => {
+  console.log('post SIGN UP');
   const { firstName, lastName, email, password } = req.body;
   console.log(firstName, lastName, email, password);
   if (!firstName || !lastName || !email || !password) {
@@ -54,6 +55,43 @@ app.post('/api/register', (req, res, next) => {
           res.status(201).json(newUser);
         })
         .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+//sign in
+app.post('/api/auth/sign-in', (req, res, next) => {
+  console.log('post SIGN IN')
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ClientError(401, 'invalid login MISSING EMAIL OR PW');
+  }
+
+  const sql = `
+        select  "userId",
+                "password"
+        from "users"
+        where "email" = $1
+      `;
+  const params = [email];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      console.log('111', user)
+      if (!user) {
+        throw new ClientError(401, 'invalid login USER NO EXIST');
+      }
+      const {userId} = user;
+      return argon2
+        .verify(user.password, password)
+        .then(isMatching => {
+          console.log('does your password match?', isMatching);
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login WRONG PASSWORD');
+          }
+          const payload = {userId, email};
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.status(200).json({ payload, token });
+        })
     })
     .catch(err => next(err));
 });
