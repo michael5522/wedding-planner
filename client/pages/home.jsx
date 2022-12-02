3417da6e52740ac192aa2208c31c5f3320ca2152
Michael Chang <michaelchang.kuo@gmail.com>
2022-12-02 00:15:27 +0000

    got some boxes working

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -81,52 +81,50 @@ export default class Home extends React.Component {
         </div>
 
         <MenuInfo />
-        {/* <div className="row custom-row">
-          <div className="col d-flex align-items-center justify-content-center">
-            <img src="https://images.unsplash.com/photo-1535254973040-607b474cb50d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzE1fHx3ZWRkaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" className="custom-row-photo" alt="Responsive image" />
-          </div>
 
-          <div className="col">
-            <div>
-              <h3 className="mb-0">Redefining the</h3>
-              <h3 className="mb-0">Event Planning</h3>
-              <h3 className="mb-10">Experience</h3>
-              <p className="mt-3">
-                Weddings are full of love, dancing and…planning. There’s a lot of decisions and steps, but with our checklist, you can make sure you have it all! <br />
-                Inside the included tools, <strong>you can set your budget.</strong> It’s highly advised this be one of the very first priorities. Why? It’ll set the precedent for everything else.<br />
-                You may also <strong>put together the guest list. </strong>Why do you have to do this early? To know approximately what sized wedding venue you’ll be booking! <br />
-                <strong>Hiring a planner? </strong>Start with them early on; they have the experience to make the overall process easier.
-              </p>
+        <div className="row align-items-center justify-content-center">
+          <h3>Example Benefits:</h3>
+        </div>
+        <div className="row custom-row-2">
 
+          <div className="col-md-4 d-flex align-items-stretch justify-content-center menu-top-margin">
+            <div className="card border-dark mb-3">
+              <div className="card-header">Guest List Manager</div>
+              <div className="card-body text-dark">
+                <p className="card-text">Simplify address collection. <br />
+                  Whether you’re having a wedding shower, engagement party or dinner, our guest list manager will assemble all your guest info.</p>
+              </div>
             </div>
 
           </div>
 
-        </div> */}
+          <div className="col-md-4 d-flex align-items-stretch justify-content-center menu-top-margin">
+            <div className="card border-dark mb-3">
+              <div className="card-header">A Wedding To-Do List</div>
+              <div className="card-body text-dark">
+                <p className="card-text">Add. Remove. Update. <br />
+                  Your wedding to-do list is what you make it.
+                  Easily update or change to fit your needs.</p>
+              </div>
+            </div>
 
-        <div className="row">
-          <h3>Example Benefits:</h3>
-        </div>
-        <div className="row bg-light">
-          <div className="col-md-4 border rounded">
-            <h4>Guest List Manager</h4>
-            <h6 className="italic">Simplify address collection</h6>
-            <p className="text-justify"><br />Whether you’re having a wedding shower, bach party, engagement party or rehearsal dinner, our guest list manager was thoughtfully designed to help you gather guest info and keep it organized for any and all of your events.</p>
           </div>
 
-          <div className="col-md-4 border rounded">
-            <h4>Wedding Checklist</h4>
-            <h6 className="italic">A Wedding To-Do List</h6>
-            <p className="text-justify"><br />Add. Remove. Update. <br />
-              Your wedding to-do list is what you make it. <br />
-              Easily update or change to fit your needs.</p>
-          </div>
-
-          <div className="col-md-4 border rounded">
+          {/* <div className="col-md-4 border rounded">
             <h4>Budgeter</h4>
             <h6 className="italic">Set up your Budget!</h6>
             <p className="text-justify"><br />Stay on top of yours spending - keep track of your total budget, prices, deposits, and any outstanding fees</p>
+          </div> */}
+
+          <div className="col-md-4 d-flex align-items-stretch justify-content-center menu-top-margin">
+            <div className="card border-dark mb-3">
+              <div className="card-header">Set up your Budget!</div>
+              <div className="card-body text-dark">
+                <p className="card-text">Stay on top of yours spending -<br /> Keep track of your total budget, the prices of everything, and any further outstanding fees!</p>
+              </div>
+            </div>
           </div>
+
         </div>
 
       </div>
