439bf62709b6e6bff71167c913331fded6502ed3
Michael Chang <michaelchang.kuo@gmail.com>
2022-12-10 01:25:06 +0000

    added picture and css

--- client/pages/faq.jsx
+++ client/pages/faq.jsx
@@ -2,65 +2,104 @@ import React from 'react';
 
 export default function Faq(props) {
   return (
-    <div className="container">
+    <div>
 
-      <section>
-        <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" className="rounded img-fluid img-half mx-auto d-block mb-4" alt="Responsive image" />
-        <h3 className="text-center mb-2 pb-2 text-secondary fw-bold">FAQ</h3>
-        <p className="text-center mb-2">
-          Find the answers for the most frequently asked questions below
-        </p>
-        <a className="d-flex justify-content-center btn btn-outline-secondary mb-2" href="#menu"> Return to Menu</a>
-
-        <div className="row">
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="far fa-paper-plane text-muted pe-2" /> Do I need a wedding planner?</h6>
-            <p>
-              <strong><u>Absolutely</u></strong> not! We believe that a wedding planner is nice to have but not necessary.Some may be incredible at their jobs, and will go above and beyond, but most will not.
-            </p>
-          </div>
-
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="fas fa-pen-alt text-muted pe-2" /> What Does A Venue Coordinator Do?</h6>
-            <p>
-              Their job is to be your <strong><u>venue liaison, and ensure the smooth running of your day</u></strong> - but only for things that fall under the remit of the venue.
-              Some venue coordinators will work with a local stylist or rental company on custom choices like centrepieces or chair coverings.
-
-            </p>
-          </div>
-
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="fas fa-user text-muted pe-2" /> How much does a wedding planner cost?
-            </h6>
-            <p>
-              <strong><u>The cost of wedding planner can differ wildly depending on their experience</u></strong>, what they offer, and the kind of wedding you&apos; re having. Some will charge an hourly rate anywhere from $40 to $200, others will offer a package that includes a certain number of calls, in-person meetings as well as their day-of service, and others will charge a rate depending on your overall budget, usually 10-15%.
-            </p>
-          </div>
-
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="fas fa-rocket text-muted pe-2" /> Do you have to pay a wedding coordinator?
-            </h6>
-            <p>
-              <strong><u>A venue coordinator is complimentary with the venue</u></strong>, though it is nice to leave a gratuity or give them a gift, or a thank-you card, if you&apos; re happy with their service.
-            </p>
-          </div>
-
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="fas fa-home text-muted pe-2" /> What Does A Wedding Stylist Do?
-            </h6>
-            <p><strong><u>A wedding stylist will design and decorate your wedding for you.</u></strong> A wedding stylist&apos; s role can be as in-depth as a creative director, and involve coming up with a colour palette and concept for your wedding. At other weddings, their role is as simple as executing the set-up on the day, and making sure everything is place.</p>
-          </div>
-
-          <div className="col-md-6 col-lg-4 mb-4">
-            <h6 className="mb-3 text-dark"><i className="fas fa-book-open text-muted pe-2" /> What Does A Wedding Planner Do?
-            </h6>
-            <p>
-              <strong><u>A full-service wedding planner will help you find the right venue, source suppliers, realise your vision and stay in budget and that&apos; s all <i>before</i> the big day.</u></strong> On the day itself, they and their team will set the venue up for you, they&apos; ll keep tabs on all the suppliers, and they&apos; ll ensure you never hear about any last minute near-disasters.On the big day, you don&apos;t have to worry about a thing.
-            </p>
-          </div>
+      <div className="position-relative overflow-hidden text-center menu-row">
+        <div className="col-md-5 p-lg-5 mx-auto my-5">
+          <h1 className="display-4 font-weight-normal lustria-font menu-text">Frequently Asked Questions</h1>
+          <p className="lead font-weight-normal lustria-font italic menu-text">Find the answers for the most asked questions below!</p>
+          <a className="btn btn-outline-secondary lustria-font menu-text" href="#menu">Return to Menu</a>
         </div>
-      </section>
+      </div>
+
+      <div className="home-ultrabackground-9 p-1 mb-0 mt-0 d-flex container-fluid" />
+
+      <div className="faq-background-fixed mt-0">
+        <div className="container">
+
+          <section>
+            <img src="/images/faq.png" className="img-fluid img-twenty-five mx-auto d-block mb-4" alt="Responsive image" />
+
+            <div className="row">
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="far fa-paper-plane blue-color pe-2" /> <u>Do I need a wedding planner?</u></h6>
+                <p>
+                  <strong>Absolutely</strong> not! We believe that a wedding planner is nice to have but not necessary.Some may be incredible at their jobs, and will go above and beyond, but most will not.
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-pen-alt blue-color pe-2" /> <u>What Does A Venue Coordinator Do?</u></h6>
+                <p>
+                  Their job is to be your <strong>venue liaison, and ensure the smooth running of your day</strong> - but only for things that fall under the remit of the venue.
+                  Some venue coordinators will work with a local stylist or rental company on custom choices like centrepieces or chair coverings.
+
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-user blue-font pe-2" /> <u>How much does a wedding planner cost?</u>
+                </h6>
+                <p>
+                  <strong>The cost of wedding planner can differ wildly depending on their experience</strong>, what they offer, and the kind of wedding you&apos; re having. Some will charge an hourly rate anywhere from $40 to $200, others will offer a package that includes a certain number of calls, in-person meetings as well as their day-of service, and others will charge a rate depending on your overall budget, usually 10-15%.
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-rocket blue-font pe-2" /> <u>Do you have to pay a wedding coordinator?</u>
+                </h6>
+                <p>
+                  <strong>A venue coordinator is complimentary with the venue</strong>, though it is nice to leave a gratuity or give them a gift, or a thank-you card, if you&apos; re happy with their service.
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-home blue-font pe-2" /> <u>What Does A Wedding Stylist Do?</u>
+                </h6>
+                <p><strong>A wedding stylist will design and decorate your wedding for you.</strong> A wedding stylist&apos; s role can be as in-depth as a creative director, and involve coming up with a colour palette and concept for your wedding. At other weddings, their role is as simple as executing the set-up on the day, and making sure everything is place.</p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-book-open blue-font pe-2" /> <u>What Does A Wedding Planner Do?</u>
+                </h6>
+                <p>
+                  <strong>A full-service wedding planner will help you find the right venue, source suppliers, realise your vision and stay in budget and that&apos; s all <i>before</i> the big day.</strong> On the day itself, they and their team will set the venue up for you, they&apos; ll keep tabs on all the suppliers, and they&apos; ll ensure you never hear about any last minute near-disasters.On the big day, you don&apos;t have to worry about a thing.
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-boxes blue-font pe-2" /> <u>What does a venue coordinator do?</u>
+                </h6>
+                <p>
+                  <strong>A venue coordinator oversees the on site staff</strong>, to make sure they are fulfilling their contractual obligations, serving food on time, etc. A lot of times the venue coordinator is not there for the duration of the event &/or they have other events they are overseeing on property during the same time
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-circle-notch blue-font pe-2" /> <u>What Does A Wedding wedding coordinator do?</u>
+                </h6>
+                <p>
+                  <strong>A wedding coordinator works only for you to make every aspect run smoothly</strong>, greet all your vendors, send out a timeline prior, make sure you are having a great time celebrating and to ensure that your vision has become a reality.
+                </p>
+              </div>
+
+              <div className="col-md-6 col-lg-4 mb-4">
+                <h6 className="mb-3 blue-color mate-font"><i className="fas fa-hands-helping blue-font pe-2" /> <u>Why should you hire a wedding coordinator?</u>
+                </h6>
+                <p>
+                  <strong>To truly enjoy their wedding day</strong>. To invest in their friends, family & guests without worrying about the details themselves. To have a peace of mind that everything will run smoothly. Wedding coordinators are professionals who have experience and industry relationships that will allow the brides wedding vision come to life.
+                </p>
+              </div>
+            </div>
+          </section>
+
+        </div>
+      </div>
+
+      <div className="home-ultrabackground p-1 mb-0 mt-0 d-flex container-fluid" />
+      <div className="home-black d-flex p-5 flex-grow-1 container-fluid" />
 
     </div>
+
   );
 }
